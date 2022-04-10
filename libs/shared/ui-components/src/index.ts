df08e94065c8bd1bf415294b979eb4099bd70514
fzum <sellemondfabian@gmail.com>
2022-04-10 13:06:01 +0200

    fix build error

--- libs/shared/ui-components/src/index.ts
+++ libs/shared/ui-components/src/index.ts
@@ -1,2 +1,3 @@
 export * from './lib/shared-ui-components.module';
 export { CardConfiguration } from './lib/card/card-configuration.model';
+export { CardComponent } from './lib/card/card.component';
