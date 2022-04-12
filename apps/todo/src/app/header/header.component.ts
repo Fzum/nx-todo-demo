973f42a7ba029b337d8b411af02803403d162d26
fzum <sellemondfabian@gmail.com>
2022-04-12 21:43:21 +0200

    add header information relative to router position

--- apps/todo/src/app/header/header.component.ts
+++ apps/todo/src/app/header/header.component.ts
@@ -1,7 +1,34 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter, map } from 'rxjs';
+
+const defaultHeading = 'Welcome to NX Todo';
 
 @Component({
   selector: 'nx-todo-demo-header',
   templateUrl: './header.component.html',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  heading = defaultHeading;
+
+  private urlHeadingMap = new Map<string, string>([
+    ['view', 'View your Todos'],
+    ['manage', 'Manage your Todos'],
+  ]);
+
+  constructor(private router: Router) {
+    router.events
+      .pipe(
+        filter((event) => event instanceof NavigationEnd),
+        map((e) => e as NavigationEnd)
+      )
+      .subscribe((event) => {
+        const keys = [...this.urlHeadingMap.keys()];
+        const urlKey = keys.find((k) => event.url.includes(k));
+
+        this.heading = urlKey
+          ? (this.urlHeadingMap.get(urlKey) as string)
+          : defaultHeading;
+      });
+  }
+}
